6d63e2b33689bee54b7b3500a61d3fd05949cf9c
Alejandro Marin <almarinca@unal.edu.co>
2025-03-01 21:04:55 -0500

    socket io url from env variable

--- src/SpeechTranslator.js
+++ src/SpeechTranslator.js
@@ -2,7 +2,7 @@ import './index.css';
 import { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("/api");
+const socket = io(`${process.env.REACT_APP_API_URL}`);
 
 export default function SpeechTranslator() {
   const [partialTranscript, setPartialTranscript] = useState("");
