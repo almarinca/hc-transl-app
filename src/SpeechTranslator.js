deb95343386e7f3aa8cd5ba5a66bf0da714c9556
Alejandro Marin <almarinca@unal.edu.co>
2025-03-01 21:34:02 -0500

    revert the last fixes

--- src/SpeechTranslator.js
+++ src/SpeechTranslator.js
@@ -2,7 +2,7 @@ import './index.css';
 import { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("/api");
+const socket = io(`${process.env.REACT_APP_API_URL}`);
 
 export default function SpeechTranslator() {
   const [partialTranscript, setPartialTranscript] = useState("");
@@ -22,7 +22,7 @@ export default function SpeechTranslator() {
   const intervalRef = useRef(null); // Store interval reference
 
   useEffect(() => {
-    fetch("/api/languages")
+    fetch(`${process.env.REACT_APP_API_URL}/languages`)
       .then((response) => response.json())
       .then((data) => setLanguages(data))
       .catch((error) => console.error("Error fetching languages:", error));
